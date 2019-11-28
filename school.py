3e5865ea3bfcfa698707257d63efeacf3d8f0520
umar khan <umarkhan314@outlook.com>
2019-11-28 23:25:03 +0000

    objatts

--- school.py
+++ school.py
@@ -1,1 +1,14 @@
 class School:
+	def __init__(self,name,roster = {}):
+		self.name=name
+		self.roster=roster
+
+	def add_student(self,name,grade):
+		if grade not in self.roster.keys():
+			self.roster[grade]=[]
+		self.roster[grade].append(name)
+	def grade(self,grade):
+		return(self.roster[grade])
+	def sort_roster(self):
+		for i in self.roster.keys():
+			self.roster[i].sort()
